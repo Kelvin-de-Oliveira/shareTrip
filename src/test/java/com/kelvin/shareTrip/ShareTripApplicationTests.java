edca951c1c5f3e4e8745831c0e0073d8de431419
Kelvin Oliveira <kelvin.oliveira.0110@gmail.com>
2024-07-22 23:47:07 -0300

    Configuração ambiente desenvolvimento

---
+++ src/test/java/com/kelvin/shareTrip/ShareTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelvin.shareTrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
