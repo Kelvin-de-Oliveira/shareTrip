6c1a4588d46da92db35154c0c07c4ee906be40d3
GiovannaLyssa99 <giovannalyssa@discente.ufg.br>
2024-07-25 04:49:55 -0300

    feat: criacao da entidade Relato

--- src/main/java/com/kelvin/shareTrip/model/Relato.java
+++ src/main/java/com/kelvin/shareTrip/model/Relato.java
@@ -1,5 +1,24 @@
 package com.kelvin.shareTrip.model;
 
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Document(collection = "shareTrip")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Relato {
-    
+    @Id
+    private String id;
+    private String descricao;
+    private String foto;
+    private Date data;
+    //@DBRef
+    //private Usuario criador;
+    //@DBRef
+    //private Destino destino;
 }
