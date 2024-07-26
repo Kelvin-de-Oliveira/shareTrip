2c38350b54616d0df2c628cc616409d3b8ded4e3
Abraao Santiago Moreira <santiagoabraao17@gmail.com>
2024-07-25 22:15:17 -0300

    feat: implementacao destinoController

--- src/main/java/com/kelvin/shareTrip/controller/ServiceController.java
+++ src/main/java/com/kelvin/shareTrip/controller/ServiceController.java
@@ -2,6 +2,8 @@ package com.kelvin.shareTrip.controller;
 
 import com.kelvin.shareTrip.model.Relato;
 import com.kelvin.shareTrip.service.RelatoService;
+import com.kelvin.shareTrip.model.Destino;
+import com.kelvin.shareTrip.service.DestinoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -36,5 +38,36 @@ public class ServiceController {
     @PutMapping("/relato/update/{id}")
     public Relato update(@PathVariable String id, @RequestBody Relato relato) {
         return relatoService.updateRelato(id, relato);
+    };
+
+
+    // Destino 
+
+    @Autowired
+    private DestinoService destinoService;
+    
+    @GetMapping("/destino/all")
+    public List<Destino> getAllDestino() {
+        return destinoService.getAllDestino();
+    };
+
+    @GetMapping("/destino/{id}")
+    public Destino getDestinoById(@PathVariable String id) {
+        return destinoService.getDestinoById(id);
+    };
+
+    @PostMapping("/destino/add")
+    public Destino addDestino(@RequestBody Destino destino){
+        return destinoService.addDestino(destino);
+    };
+
+    @DeleteMapping("/destino/delete/{id}")
+    public Destino deleteDestino(@PathVariable String id) {
+        return destinoService.deleteDestino(id);
+    };
+
+    @PutMapping("/destino/update/{id}")
+    public Destino updateDestino(@PathVariable String id, @RequestBody Destino destino) {
+        return destinoService.updateDestino(id, destino);
     };
 }
